67f6cfb10024c3f51fa8b828485b8fe9efd8d86a
Felipe Acquati <unomais@yahoo.com.br>
2018-10-29 19:35:21 +0000

    Added: customers module and component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomersModule } from './customers/customers.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, CustomersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
