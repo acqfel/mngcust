fc685d1806624d032b780c7195b438a729143360
Felipe Acquati <unomais@yahoo.com.br>
2018-10-30 02:02:25 +0000

    Added: shared module, shared interfaces; Updated: filteredCustomers type [list-customers], people type [customers component].

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CustomersModule } from './customers/customers.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, CustomersModule
+    BrowserModule, CustomersModule, SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/customers/list-customers/list-customers.component.ts
+++ src/app/customers/list-customers/list-customers.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ICustomer } from '../../shared/interfaces';
+
 @Component({
   selector: 'app-list-customers',
   templateUrl: './list-customers.component.html',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListCustomersComponent implements OnInit {
   
-  filteredCustomers: any[] = [];
+  filteredCustomers: ICustomer[] = [];
   customersOrderTotal: number;
   currencyCode: string = 'USD';
 
