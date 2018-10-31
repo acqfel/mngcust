5f0d6e6cc4421851ad0cb92de89eb0640895c180
Felipe Acquati <unomais@yahoo.com.br>
2018-10-31 02:08:17 +0000

    Added: filter method to list-customers coponent, @Input @Output to input-filter component.

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { CustomersComponent } from './customers.component';
 import { ListCustomersComponent } from './list-customers/list-customers.component';
 import { InputFilterComponent } from './list-customers/input-filter/input-filter.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [
     CustomersComponent,
--- src/app/customers/list-customers/input-filter/input-filter.component.ts
+++ src/app/customers/list-customers/input-filter/input-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-input-filter',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputFilterComponent implements OnInit {
 
+  private _filter: string;
+  @Input() get filter() {
+    return this._filter;
+  }
+  
+  set filter(val: string) {
+    this._filter = val;
+    this.changed.emit(this.filter);
+  }
+
+  @Output() changed: EventEmitter<string> = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit() {
   }
+  
+  
 
 }
