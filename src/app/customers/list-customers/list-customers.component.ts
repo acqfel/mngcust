0255ef3b192ada4ea37fa05171c0b1a010a0829e
Felipe Acquati <unomais@yahoo.com.br>
2018-10-31 01:02:03 +0000

    Added: @Input to list-customers, click events to template.

--- src/app/customers/list-customers/list-customers.component.ts
+++ src/app/customers/list-customers/list-customers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { ICustomer } from '../../shared/interfaces';
 
@@ -8,6 +8,17 @@ import { ICustomer } from '../../shared/interfaces';
   styleUrls: ['./list-customers.component.scss']
 })
 export class ListCustomersComponent implements OnInit {
+  private _customers: ICustomer[] = [];
+  @Input() get customers(): ICustomer[] {
+    return this._customers;
+  }
+  
+  set customers(value: ICustomer[]) {
+    if (value) {
+      this.filteredCustomers = this._customers = value;
+      this.calculateOrders();
+    }
+  }
   
   filteredCustomers: ICustomer[] = [];
   customersOrderTotal: number;
@@ -16,12 +27,7 @@ export class ListCustomersComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.filteredCustomers = [
-            { id: 1, name: 'john Doe', city: 'Phoenix', orderTotal: 9.99, customerSince: new Date(2014, 7, 10) },
-            { id: 2, name: 'Jane Doe', city: 'Chandler', orderTotal: 19.99, customerSince: new Date(2017, 2, 22)},
-            { id: 3, name: 'Michelle Thomas', city: 'Seattle', orderTotal: 99.99, customerSince: new Date(2002, 10, 31)},
-            { id: 4, name: 'Jim Thomas', city: 'New York', orderTotal: 599.99, customerSince: new Date(2002, 10, 31)},
-        ];
+
   }
   
   calculateOrders() {
@@ -30,5 +36,9 @@ export class ListCustomersComponent implements OnInit {
           this.customersOrderTotal += cust.orderTotal;
       });
     }
+    
+  sort(prop: string){
+    // sorter service
+  }
 
 }
