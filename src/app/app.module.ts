6a324a55de58383f9be024c59bf27eab4b5a0376
Felipe Acquati <unomais@yahoo.com.br>
2018-11-02 18:53:15 +0000

    Added: orders module & component, orders-routing; Updated: list-customers template - routerLink.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { CustomersModule } from './customers/customers.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { OrdersModule } from './orders/orders.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, CustomersModule, SharedModule, CoreModule, AppRoutingModule
+    BrowserModule,
+    CustomersModule,
+    OrdersModule,
+    SharedModule,
+    CoreModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
